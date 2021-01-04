0b3ba79884e8743e7356ef41f908a75e5e69c6a6
yaikohi <erik.beem@student.hu.nl>
2021-01-04 10:46:44 +0100

    - Changed the text in ChartOverview.

--- src/components/histogram/ChartOverview.jsx
+++ src/components/histogram/ChartOverview.jsx
@@ -10,11 +10,11 @@ export class ChartOverview extends Component {
         return (
             <div>
                 <Heading level="h1" as="h2" margin="0 0 x-small"> Placeholder Histogram</Heading>
-                <p>Deze histogram laat de toetsresultaten zien van vier verschillende toetsen.</p>
+                <p>Deze histogram laat de toetsresultaten zien van alle toetsen van een jaar.</p>
                 <p>
                     Het is mogelijk om alleen 1 soort resultaat te laten zien (alleen voldoendes of alleen onvoldoendes per toets).
                     <br/>
-                    Klik op "voldoendes" of "onvoldoendes" bovenin de grafiek.
+                    Klik op "voldoendes" of "onvoldoendes" bovenin de grafiek om deze te laten verdwijnen of ze weer terug te halen.
                 </p>
                 <tests />
                 <Histogram />
